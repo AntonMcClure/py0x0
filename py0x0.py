ed8a73cbc105d79fe2cc77c12298ec3625f6bccb
Anton McClure <anton@antonmcclure.com>
2019-12-30 23:07:20 +0000

    fix requests license

--- py0x0.py
+++ py0x0.py
@@ -160,20 +160,22 @@ def cmdShowLicenseLachs0r():
 
 def cmdShowLicenseRequests():
     clear()
-    
+    print("=========================================================================")
+    print("    py0x0 | Requests Software License")
+    print("=========================================================================")
     print('Copyright 2019 Kenneth Reitz')
-
-    print('   Licensed under the Apache License, Version 2.0 (the "License");
-    print('   you may not use this file except in compliance with the License.
-    print('   You may obtain a copy of the License at
     print()
-    print('       https://www.apache.org/licenses/LICENSE-2.0
+    print('   Licensed under the Apache License, Version 2.0 (the "License");')
+    print('   you may not use this file except in compliance with the License.')
+    print('   You may obtain a copy of the License at')
     print()
-    print('   Unless required by applicable law or agreed to in writing, software
-    print('   distributed under the License is distributed on an "AS IS" BASIS,
-    print('   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    print('   See the License for the specific language governing permissions and
-    print('   limitations under the License.
+    print('       https://www.apache.org/licenses/LICENSE-2.0')
+    print()
+    print('   Unless required by applicable law or agreed to in writing, software')
+    print('   distributed under the License is distributed on an "AS IS" BASIS,')
+    print('   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.')
+    print('   See the License for the specific language governing permissions and')
+    print('   limitations under the License.')
 
     cmdPause()
     main()
